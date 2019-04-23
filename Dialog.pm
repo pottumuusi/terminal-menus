10beb179eca706a8eceb43f6300d089732f243ed
Aleksi Backman <bddubbb@gmail.com>
2019-04-23 23:30:03 +0300

    Move dialog logic to own module

--- Dialog.pm
+++ Dialog.pm
@@ -3,8 +3,89 @@ package Dialog;
 use warnings;
 use strict;
 
-sub test() {
-	print "This is a test\n";
+use constant DEBUG => 1;
+
+use constant DIALOG_OK => 0;
+use constant DIALOG_CANCEL => 1;
+use constant DIALOG_HELP => 2;
+use constant DIALOG_EXTRA => 3;
+use constant DIALOG_ITEM_HELP => 4;
+use constant DIALOG_ESC => 255;
+use constant DIALOG_ERR => 256;
+
+our $init_done = 0;
+our $fh_comm = undef;
+our $dialog_comm_file = undef;
+
+sub init {
+	$fh_comm = File::Temp->new();
+	$dialog_comm_file = $fh_comm->filename;
+
+	# TODO check that dialog_comm_file exists(?)
+
+	if (DEBUG) {
+		print("dialog_comm_file is: $dialog_comm_file\n");
+	}
+
+	$init_done = 1;
+
+	return 0;
+}
+
+sub teardown {
+	if (1 != $init_done) {
+		print("Teardown called when init not done\n");
+		return 0;
+	}
+
+	if (defined $fh_comm) {
+		close($fh_comm); # TODO handle close failure(?)
+	}
+
+	$init_done = 0;
+
+	return 0;
+}
+
+sub init_ok {
+	# TODO Add necessary checks
+
+	if (! defined $dialog_comm_file) {
+		return 0;
+	}
+
+	return 1;
+}
+
+sub input_box {
+	# TODO Give height and width as named parameters
+	my $height = $_[0];
+	my $width = $_[1];
+
+	my $ret = DIALOG_ERR;
+	my $result = undef;
+	my $dialog_cmd = undef;
+
+	if (!init_ok()) {
+		print("Dialog not initialized properly\n");
+		return -1;
+	}
+
+	# Construct dialog command which passes user input via temp file.
+	$dialog_cmd = "dialog ";
+	$dialog_cmd = $dialog_cmd . "--title 'INPUT BOX' ";
+	$dialog_cmd = $dialog_cmd . "--clear ";
+	$dialog_cmd = $dialog_cmd . "--inputbox 'Hello' $height $width ";
+	$dialog_cmd = $dialog_cmd . "2> $dialog_comm_file";
+	$ret = system("$dialog_cmd");
+
+	$result = "";
+	while (my $row = <$fh_comm>) {
+		chomp $row;
+		$result = $result . $row;
+	}
+
+	return $result;
 }
 
 1;
--- menu_main.pl
+++ menu_main.pl
@@ -7,35 +7,11 @@ use File::Temp;
 
 use Dialog;
 
-my $dialog_comm_pipe = "comm_pipe";
-my $dialog_comm_pipe_permissions = 0666;
-my $ret;
-my $tempfilename;
-my $fh_comm = File::Temp->new();
-my $dialog_comm_file = $fh_comm->filename;
+my $dialog_result = undef;
 
-use constant DIALOG_OK => 0;
-use constant DIALOG_CANCEL => 1;
-use constant DIALOG_HELP => 2;
-use constant DIALOG_EXTRA => 3;
-use constant DIALOG_ITEM_HELP => 4;
-use constant DIALOG_ESC => 255;
+Dialog::init();
 
-Dialog::test();
+$dialog_result = Dialog::input_box(16, 51);
+print("dialog_result is: $dialog_result\n");
 
-print "dialog_comm_file is: $dialog_comm_file\n";
-
-print("Showing dialog\n");
-$ret = system("dialog --title 'INPUT BOX' --clear --inputbox 'Hello' 16 51 2> $dialog_comm_file");
-print("\nresult of dialog command is: $ret\n");
-
-print("Reading from comm file 2\n");
-while (my $row = <$fh_comm>) {
-	chomp $row;
-	print($row);
-}
-print("\nDone reading from comm file 2\n");
-
-print "dialog_comm_file is: $dialog_comm_file\n";
-
-close($fh_comm);
+Dialog::teardown();
